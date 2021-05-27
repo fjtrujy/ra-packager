b3ee4de7ebf1c86407028c0f4d0cd197c787ab92
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-05-27 16:49:40 +0200

    Add info folder

--- package.sh
+++ package.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # package.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
 
-mkdir -p RA || { exit 1; }
-mkdir -p RA/cores || { exit 1; }
+./prepare_release.sh || { exit 1; }
 
 ./prepare_retroarch.sh || { exit 1; }
 
@@ -13,6 +12,9 @@ mkdir -p RA/cores || { exit 1; }
 #libretro-samples
 ./libretro-samples.sh || { exit 1; }
 ./generate_retroarch.sh libretro-samples test_libretro_ps2 || { exit 1; }
+
+## Copy info folder
+./libretro-core-info.sh || { exit 1; }
 
 ## Salamander to finish
 ./generate_salamander.sh
---
+++ prepare_release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# package.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
+
+mkdir -p RA || { exit 1; }
+mkdir -p RA/cores || { exit 1; }
+mkdir -p RA/info || { exit 1; }
