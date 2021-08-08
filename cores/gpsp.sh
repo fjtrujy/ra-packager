55375ea648e1ce94dcc07bacc4ab3434af8039fc
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-08-08 18:25:40 +0200

    Try HDD support

--- cores/gpsp.sh
+++ cores/gpsp.sh
@@ -5,7 +5,7 @@
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 ## Download the source code.
-REPO_URL="https://github.com/davidgfnet/gpsp"
+REPO_URL="https://github.com/libretro/gpsp"
 REPO_FOLDER="gpsp"
 BRANCH_NAME="master"
 if test ! -d "$REPO_FOLDER"; then
